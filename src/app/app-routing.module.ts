fb1f0a38243db1d5aa26f8920d83861d0877e8b4
chengyumeng <792400644@qq.com>
2019-09-29 11:28:42 +0800

    fix route error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 
-const routes: Routes = [ { path: '**', component: AppComponent } ];
+const routes: Routes = [ { path: '**', component: AppComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
